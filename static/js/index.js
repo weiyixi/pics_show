7fb396f8148377a87f2544508cc5f6fe4d56f5d6
weiyixi <weiyixi@koudailc.com>
2020-02-01 18:38:46 +0800

    pic

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,7 @@
   var u = navigator.userAgent;
   var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
   var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-  if ((isAndroid || isiOS) && window.location.href.indexOf('admin') !== -1) {
+  if ((isAndroid || isiOS) && window.location.href.indexOf('admin/') !== -1) {
     window.location.href = '/';
     return;
   }
